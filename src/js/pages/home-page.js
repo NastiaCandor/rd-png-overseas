76cf2f6381518def8933e6d736361f270e2914ca
NastiaCandor <andubova98@gmail.com>
2023-11-14 17:06:17 +0300

    feat: add services section on home page desc style

--- src/js/pages/home-page.js
+++ src/js/pages/home-page.js
@@ -1,3 +1,9 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation} from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 // Hide construction img
 const options = { threshold: [0.5] };
 const constructObserver = new IntersectionObserver(approachContractionShow, options);
@@ -12,3 +18,79 @@ function approachContractionShow(entry) {
 }
 
 constructObserver.observe(constructionImg);
+
+// Swiper services
+
+const servicesSwiperBlock = document.querySelector('.services__slider');
+
+// First option slider
+
+// if (servicesSwiperBlock) {
+//   const servicesSwiper = new Swiper('.services__slider', {
+//     modules: [Navigation],
+//     slidesPerView: 'auto',
+//     // slidesPerView: 4,
+//     centeredSlides: true,
+//     // cssMode: true,
+//     spaceBetween: '2%',
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.services__swiper-btn-next',
+//       prevEl: '.services__swiper-btn-prev',
+//     },
+//     effect: 'creative',
+//     creativeEffect: {
+//       prev: {
+//         // will set `translateX(100%)` on next slides
+//         translate: ['100%', 0, 0],
+//       },
+//       next: {
+//         // will set `translateX(100%)` on next slides
+//         translate: ['100%', 0, 0],
+//       },
+//     },
+//   });
+// }
+
+// Second option
+
+if (servicesSwiperBlock) {
+  const servicesSwiper = new Swiper('.services__slider', {
+    modules: [Navigation],
+    slidesPerView: 'auto',
+    spaceBetween: '2%',
+    navigation: {
+      nextEl: '.services__swiper-btn-next',
+      prevEl: '.services__swiper-btn-prev',
+    },
+  });
+}
+
+const optionsSliderObserver = { 
+  threshold: [0],
+  // root: null,
+  // root: document.querySelector('.swiper-wrapper'),
+  // rootMargin: '10px 0',
+};
+const servicesSlidesObserver = new IntersectionObserver(hideServicesSlides, optionsSliderObserver);
+const servicesSlides = document.querySelectorAll('.swiper-slide');
+console.log(servicesSlides);
+
+function hideServicesSlides(entry) {
+  console.log(entry);
+  entry.forEach(slide => {
+    if (slide.isIntersecting) {
+      // slide.target.classList.add('hidden');
+      slide.target.classList.remove('hidden');
+    } else {
+      // slide.target.classList.remove('hidden');
+      slide.target.classList.add('hidden');
+    }
+  });
+}
+
+servicesSlides.forEach(slide => {
+  // slide.classList.remove('hidden');
+  slide.classList.add('hidden');
+  servicesSlidesObserver.observe(slide);
+});
