3c053c36ad52419d66f386be56e2caa5f9e0902b
NastiaCandor <andubova98@gmail.com>
2023-12-13 01:07:05 +0300

    feat: add mob style to career page

--- src/js/pages/career.js
+++ src/js/pages/career.js
@@ -1,5 +1,54 @@
 import $ from "jquery";
+import Swiper from 'swiper';
+import { Navigation} from 'swiper/modules';
+// CARING section
+$(document).ready(function() {
+  if (!document.querySelector('.caring')) return;
+  let caringSwiper;
 
+  if (!$(".caring__swiper").data("platform")) {
+		screen.width < 769
+			? $(".caring__swiper").data("platform", "mobile")
+			: $(".caring__swiper").data("platform", "desktop");
+	}
+  if (screen.width < 769) {
+    caringSwiper = new Swiper('.caring__swiper', {
+      modules: [Navigation],
+      slidesPerView: 1,
+      speed: 400,
+      navigation: {
+        nextEl: '.caring__swiper-btn-next',
+        prevEl: '.caring__swiper-btn-prev',
+      },
+    });
+  }
+
+  $(window).resize(function () {
+		changeSlider();
+	});
+
+  function changeSlider() {
+    if (screen.width < 769) {
+      if ($('.caring__swiper').data('platform') != 'desktop') return;
+      $(".caring__swiper").data("platform", "mobile");
+      if (caringSwiper) caringSwiper.destroy();
+      caringSwiper = new Swiper('.caring__swiper', {
+        modules: [Navigation],
+        slidesPerView: 1,
+        speed: 400,
+        navigation: {
+          nextEl: '.caring__swiper-btn-next',
+          prevEl: '.caring__swiper-btn-prev',
+        },
+      });
+    } else {
+      if ($('.caring__swiper').data('platform') != 'mobile') return;
+      $(".caring__swiper").data("platform", "desktop");caringSwiper.destroy();
+    }
+  }
+});
+
+// VACANCIES section
 $(document).ready(function () {
   if (!document.querySelector('.vacancies')) return;
   // FILTER FUNCTIONALITY
@@ -8,9 +57,15 @@ $(document).ready(function () {
       const id = e.target.getAttribute('id');
       const value = e.target.getAttribute('value');
       if (e.target.checked) {
+        $('.vacancies__filter-box').find(`.vacancies__filter-input#${id}`).prop('checked', true);
+        $('.modal-filter').find(`.vacancies__filter-input#${id}`).prop('checked', true);
+        $('.modal-filter').find(`.vacancies__form-label#${id}`).addClass('active');
         const item = `<div class="vacancies__filter-result-item" id="${id}">${value}<span class="vacancies__filter-close" id=${id}></span></div>`;
         $('.vacancies__filter-result').append(item);
       } else {
+        $('.vacancies__filter-box').find(`.vacancies__filter-input#${id}`).prop('checked', false);
+        $('.modal-filter').find(`.vacancies__filter-input#${id}`).prop('checked', false);
+        $('.modal-filter').find(`.vacancies__form-label#${id}`).removeClass('active');
         $('.vacancies__filter-result').find(`.vacancies__filter-result-item#${id}`).remove();
       }
     }
@@ -19,6 +74,7 @@ $(document).ready(function () {
   $('.vacancies__filter-result').click(function(e) {
     if (e.target.classList.contains('vacancies__filter-close')) {
       const id = e.target.getAttribute('id');
+      $('.modal-filter').find(`.vacancies__form-label#${id}`).removeClass('active');
       $(`.vacancies__filter-result-item#${id}`).remove();
       $(`.vacancies__filter-input#${id}`).prop('checked', false);
     }
@@ -41,17 +97,34 @@ $(document).ready(function () {
   // open filter dropdown
   $('.vacancies__filter-title').click(function(e) {
     const drop = $(this).siblings('.vacancies__filter-drop');
-    if ($('.vacancies__filter-box').hasClass('active')) {
-      drop.slideUp(300);
-      $('.vacancies__filter-box').removeClass('active');
+    if (screen.width < 768) {
+      $('.modal-filter').addClass('active');
+      $(document.body).css('overflow', 'hidden');
     } else {
-      drop.slideDown(300);
-      $('.vacancies__filter-box').addClass('active');
+      if ($('.vacancies__filter-box').hasClass('active')) {
+        drop.slideUp(300);
+        $('.vacancies__filter-box').removeClass('active');
+      } else {
+        drop.slideDown(300);
+        $('.vacancies__filter-box').addClass('active');
+      }
     }
+  });
+
+  // // modal functionality
+  $('.modal-filter').find('.modal__close').click(function() {
+    $(document.body).css('overflow', 'auto');
+    $('.modal-filter').removeClass('active');
+  });
+  $('.vacancies__form-apply-btn').click(function(e) {
+    e.preventDefault();
+    $(document.body).css('overflow', 'auto');
+    $('.modal-filter').removeClass('active');
   });
 
   // close filter menu
   document.addEventListener('click', (e) => {
+    if (screen.width < 768) return;
     if ($('.vacancies__filter-box').find(e.target).length === 0) {
       $('.vacancies__filter-box').removeClass('active');
       $('.vacancies__filter-drop').slideUp(300);
