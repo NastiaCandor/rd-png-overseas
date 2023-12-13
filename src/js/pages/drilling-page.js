aa4eb0fe9675870e65d2538a8cdf48215cdefb70
NastiaCandor <andubova98@gmail.com>
2023-12-13 18:32:32 +0300

    feat: add land-rigs slider functionality

--- src/js/pages/drilling-page.js
+++ src/js/pages/drilling-page.js
@@ -92,20 +92,45 @@ $(document).ready(function () {
 			? $(".land-rigs__info-slider").data("platform", "mobile")
 			: $(".land-rigs__info-slider").data("platform", "desktop");
 	}
+
+
+  let duration = 10,
+  sections = gsap.utils.toArray('.land-rigs__info-slide.swiper-slide'),
+  sectionIncrement = duration / (sections.length - 1),
+  tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.land-rigs',
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      start: 'top top',
+      end: '+=8000',
+      fastScrollEnd: 3000,
+    }
+  });
+  tl.to(sections, {
+    yPercent: -100 * (sections.length - 1),
+    duration: duration,
+    ease: 'none'
+  });
+
   if (screen.width > 769) {
-    // landRigsSwiper = new Swiper('.land-rigs__info-slider', {
-    //   modules: [Mousewheel],
-    //   slidesPerView: 1,
-    //   speed: 1000,
-    //   direction: "vertical",
-    //   // mousewheel: {
-    //   //   releaseOnEdges: true,
-    //   //   eventsTarget: '.land-rigs',
-    //   //   // thresholdDelta: 1,
-    //   // },
-    //   spaceBetween: 30,
-    // });
+    landRigsSwiper = new Swiper('.land-rigs__info-slider', {
+      modules: [Mousewheel],
+      slidesPerView: 1,
+      speed: 1000,
+      direction: "vertical",
+      mousewheel: {
+        releaseOnEdges: true,
+        // eventsTarget: '.land-rigs',
+        // thresholdDelta: 1,
+      },
+      spaceBetween: 30,
+    });
+    landRigsSwiper.destroy();
+
   } else {
+    tl.scrollTrigger.kill();
     landRigsSwiper = new Swiper('.land-rigs__info-slider', {
       modules: [Navigation],
       slidesPerView: 1,
@@ -125,15 +150,11 @@ $(document).ready(function () {
     if (screen.width < 769) {
       if ($('.land-rigs__info-slider').data('platform') != 'desktop') return;
       $(".land-rigs__info-slider").data("platform", "mobile");
+      tl.scrollTrigger.kill();
       landRigsSwiper.destroy();
       landRigsSwiper = new Swiper('.land-rigs__info-slider', {
         modules: [Mousewheel, Navigation],
-        slidesPerView: 2,
-        // mousewheel: {
-        //   releaseOnEdges: true,
-        //   // eventsTarget: '.land-rigs',
-        //   // thresholdDelta: 1,
-        // },
+        slidesPerView: 1,
         navigation: {
           nextEl: '.land-rigs__swiper-btn-next',
           prevEl: '.land-rigs__swiper-btn-prev',
@@ -144,54 +165,24 @@ $(document).ready(function () {
       if ($('.land-rigs__info-slider').data('platform') != 'mobile') return;
       $(".land-rigs__info-slider").data("platform", "desktop");
       landRigsSwiper.destroy();
-      // landRigsSwiper = new Swiper('.land-rigs__info-slider', {
-      //   modules: [Mousewheel],
-      //   slidesPerView: 1,
-      //   speed: 1000,
-      //   direction: "vertical",
-      //   // mousewheel: {
-      //   //   releaseOnEdges: true,
-      //   //   eventsTarget: '.land-rigs',
-      //   //   // thresholdDelta: 1,
-      //   // },
-      //   spaceBetween: 30,
-      // });
-
-      
+      tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.land-rigs',
+          pin: true,
+          scrub: 1,
+          snap: 1 / (sections.length - 1),
+          start: 'top top',
+          end: '+=8000',
+          fastScrollEnd: 3000,
+        }
+      });
+      tl.to(sections, {
+        yPercent: -100 * (sections.length - 1),
+        duration: duration,
+        ease: 'none'
+      });
     }
   }
-
-  const slides = gsap.utils.toArray('.land-rigs__info-slide.swiper-slide');
-  const landRigsWrapper = document.querySelector('.land-rigs__info-slider .swiper-wrapper');
-  const landRigsSlider = document.querySelector('.land-rigs__info-slider');
-  // const timeLine = gsap.timeline({
-  //   scrollTrigger: {
-  //     trigger: '.land-rigs',
-  //     pin: '.land-rigs',
-  //     start: 'top top',
-  //     scrub: 1,
-  //     snap: 1 / (slides.length - 1),
-  //     markers: true,
-  //     end: () => "+=" + (slides.length * slides[0].offsetWidth),
-  //   },
-  // });
-  // console.log(slides[0].offsetHeight);
-  // timeLine.to(landRigsWrapper, {
-  //   // y: -(1000),
-  //   yPercent: -100 * (slides.length - 1),
-  //   // yPercent: -100 * (4),
-  // });
-  gsap.to(slides, {
-    yPercent: -100 * (slides.length - 1),
-    ease: 'none',
-    scrollTrigger: {
-      trigger: '.land-rigs',
-      pin: true,
-      scrub: 1,
-      snap: 1 / (slides.length - 1),
-      end: () => '+=' + landRigsWrapper.offsetWidth
-    }
-  });
 
   // Show more btn
   $('.land-rigs__slide-more-btn').click(function(e) {
@@ -203,176 +194,4 @@ $(document).ready(function () {
     }
   });
 
-
-  // document.addEventListener("wheel", 
-  //   function(e) { 
-  //     if (e.wheelDeltaY > 0) {
-  //       if (landRigsSwiper.isBeginning) {
-  //         landRigsSwiper.mousewheel.disable();
-  //         var delta = e.deltaY || e.detail || e.wheelDelta;
-  //         // console.log(delta);
-  //         // $('html, body').css('transform', `translateY(${Math.abs(delta)}px)`);
-
-  //       } else {
-  //         landRigsSwiper.mousewheel.enable();
-  //       }
-  //     } else {
-  //       if (landRigsSwiper.isEnd) {
-  //         landRigsSwiper.mousewheel.disable();
-  //       } else {
-  //         landRigsSwiper.mousewheel.enable();
-  //       }
-  //     }
-  //   },{ passive: false });
-
 });
-
-
-// TRYING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-// $(document).ready(function () {
-//   if (!document.querySelector('.land-rigs')) return;
-  
-//   // const landRigsSwiper = new Swiper('.land-rigs__info-slider', {
-//   //   modules: [Mousewheel],
-//   //   slidesPerView: 1,
-//   //   speed: 1000,
-//   //   direction: "vertical",
-//   //   // mousewheel: true,
-//   //   // spaceBetween: 30,
-//   // });
-
-//   const optionsObserver = { 
-//     threshold: [0.9],
-//   };
-//   // const landRigsObserver = new IntersectionObserver(switchSlides, optionsObserver);
-
-//   // function switchSlides(entry) {
-//   //   entry.forEach(slide => {
-//   //     if (slide.isIntersecting) {
-//   //       // disableScroll();
-//   //       slide.target.classList.remove('hidden');
-//   //       document.addEventListener('wheel', please, {passive: false});
-//   //       // switchSlides();
-//   //     } else {
-//   //       // enableScroll();        slide.target.classList.remove('hidden');
-
-//   //       document.removeEventListener('wheel', please, {passive: false});
-//   //       slide.target.classList.add('hidden');
-//   //     }
-//   //   });
-//   // }
-
-//   // $(document).scroll(function () {
-//   //   console.log($('.land-rigs'));
-//   // });
-
-//   // landRigsObserver.observe(document.querySelector('.land-rigs'));
-
-//   // window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-//   // window.addEventListener(wheelEvent, preventDefault, wheelOpt);
-//   function please(event) {
-//       if (event.wheelDeltaY > 0) {
-//         console.log('up');
-//         if (landRigsSwiper.isBeginning) {
-//           console.log('dis');
-//           // enableScroll();
-//           // landRigsSwiper.mousewheel.disable();
-//         } else {
-//           console.log('on');
-//           // disableScroll();
-  
-//           event.preventDefault();
-//           landRigsSwiper.slidePrev();
-//           // landRigsSwiper.mousewheel.enable();
-//         }
-//       } else {
-//         console.log('down');
-//         if (landRigsSwiper.isEnd) {
-//           console.log('dis');
-//           // enableScroll();
-//           // landRigsSwiper.mousewheel.disable();
-//         } else {
-//           console.log('on');
-//           // disableScroll();
-//           event.preventDefault();
-//           landRigsSwiper.slideNext();
-//           // landRigsSwiper.mousewheel.enable();
-//         }
-//       }
-//   }
-
-
-//   // document.addEventListener("wheel", 
-//   // function(e) { 
-//   //   // console.log('wheel');
-//   //   e.stopImmediatePropagation();
-//   //   if (e.wheelDeltaY > 0) {
-//   //     console.log('up');
-//   //     if (landRigsSwiper.isBeginning) {
-//   //       console.log('dis');
-//   //       // enableScroll();
-//   //       // landRigsSwiper.mousewheel.disable();
-//   //     } else {
-//   //       console.log('on');
-//   //       // disableScroll();
-
-//   //       e.preventDefault();
-//   //       landRigsSwiper.slidePrev();
-//   //       // landRigsSwiper.mousewheel.enable();
-//   //     }
-//   //   } else {
-//   //     console.log('down');
-//   //     if (landRigsSwiper.isEnd) {
-//   //       console.log('dis');
-//   //       // enableScroll();
-//   //       // landRigsSwiper.mousewheel.disable();
-//   //     } else {
-//   //       console.log('on');
-//   //       // disableScroll();
-//   //       e.preventDefault();
-//   //       landRigsSwiper.slideNext();
-//   //       // landRigsSwiper.mousewheel.enable();
-//   //     }
-//   //   }
-//   // },{ passive: false });
-// });
-
-// function preventDefault(e) {
-//   e.preventDefault();
-// }
-
-// function preventDefaultForScrollKeys(e) {
-//   if (keys[e.keyCode]) {
-//     preventDefault(e);
-//     return false;
-//   }
-// }
-
-// // modern Chrome requires { passive: false } when adding event
-// var supportsPassive = false;
-// try {
-//   window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-//     get: function () { supportsPassive = true; } 
-//   }));
-// } catch(e) {}
-
-// var wheelOpt = supportsPassive ? { passive: false } : false;
-// var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-// // call this to Disable
-// function disableScroll() {
-//   console.log('dis');
-//   window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-//   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-//   // window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-//   // window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-// }
-
-// // call this to Enable
-// function enableScroll() {
-//   console.log('en');
-//   window.removeEventListener('DOMMouseScroll', preventDefault, false);
-//   window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-//   // window.removeEventListener('touchmove', preventDefault, wheelOpt);
-//   // window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-// }
